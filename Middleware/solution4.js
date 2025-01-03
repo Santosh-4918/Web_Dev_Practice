774d21e6e7007fcb15a4e8159a81c32bbb01cd8e
Santosh kumar <sakumar@descartes.com>
2025-01-04 02:32:18 +0530

    morgan added for logs

--- Middleware/solution4.js
+++ Middleware/solution4.js
@@ -1,15 +1,16 @@
 import express from "express";
 import bodyParser from "body-parser";
-import morgan from "morgan";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
-app.use(morgan("tiny"))
-app.use(bodyParser.urlencoded({extended: true}))
 var bandName = "";
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
 function bandNameGenerator(req, res, next) {
   console.log(req.body);
   bandName = req.body["street"] + req.body["pet"];
@@ -17,18 +18,15 @@ function bandNameGenerator(req, res, next) {
 }
 
 app.use(bandNameGenerator);
+
 app.get("/", (req, res) => {
-   console.log(__dirname + "/public/index.html");
   res.sendFile(__dirname + "/public/index.html");
 });
 
-// app.post("/submit",(req,res) =>{
-//   console.log(req.body);
-//   res.sendStatus(200);
-// });
 app.post("/submit", (req, res) => {
   res.send(`<h1>Your band name is:</h1><h2>${bandName}✌️</h2>`);
 });
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
